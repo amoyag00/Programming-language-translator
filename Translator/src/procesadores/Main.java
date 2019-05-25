8b9a5ef27a7e361c2ed8aea9dcc5809213054b1b
cfernv07 <cfernv07@estudiantes.unileon.es>
2019-05-25 11:45:52 +0200

    Mas pruebas

--- Translator/src/procesadores/Main.java
+++ Translator/src/procesadores/Main.java
@@ -1,76 +1,48 @@
 package procesadores;
-
 public class Main {
-	int $a = 0;
-	int $b = 0;
-
-	public static void main(String[] args) {
-		Main main = new Main();
-		main.$main();
-	}
-
-	public void $main() {
-		int $i = 0;
-		$a = 3;
-		$b = 2;
-		$i = $a - $b;
-		return;
-	}
-
-	public int $prueba1() {
-		int $i = 0;
-		$a = 2;
-		$b = 3;
-		$i = $a - $b;
-		return $i;
-	}
-
-	public int $prueba2() {
-		int $i = 0;
-		$i = 2 - 3;
-		return $i;
-	}
-
-	public int $prueba3() {
-		int $i = 0;
-		$a = 2;
-		$b = -3;
-		$i = $a - $b;
-		return $i;
-	}
-
-	public int $prueba4() {
-		int $i = 0;
-		$b = 2;
-		$i = 1 - (2 - 3);
-		return $i;
-	}
-
-	public int $prueba5() {
-		int $i = 0;
-		$i = 2;
-		$i = $i + 3;
-		return $i;
-	}
-
-	public int $prueba6() {
-		int $i = 0;
-		$i = 2;
-		$i = $i - 3;
-		return $i;
-	}
-
-	public double $prueba7() {
-		double $i = 0.0;
-		$i = 2;
-		$i = $i - 3.3;
-		return $i;
-	}
-
-	public double $prueba8() {
-		double $i = 0.0;
-		$i = 3.3 - 2.2;
-		return $i;
-	}
+double $a = 0.0;
+public class $things{
+double  $number;
+double  $number2;
+int  $number3;
+String  $cadena;
+}
+public int $hola(double $param1,double $param2){
+$param1 = $param1+$param2;
+return 0;
+}
+public static void main(String [] args){
+Main main = new Main();
+main.$main();
+}
+public void $main(){
+int $a = 0, $b = 0, $c = 0;
+double $param1 = 0.0, $param2 = 0.0;
+String $cadena = "";
+double $d = 0.0, $e = 0.0, $f = 0.0;
+int $nombre2 [] [] []= new int [2][5][4];
+boolean $k = false, $m = false;
+$things $thing = new $things();
+$things $thing2 = new $things();
+$a = 4;
+$b = 9;
+$c = 1;
+$param1 = 3.9;
+$param2 = 4.2;
+$k = true;
+$m = false;
+$thing.$number = 2.0;
+$thing.$number2 = 4.0;
+System.out.println("Escribe un número");
+while($a==$b){
+$c = $a+$b;
+}
+;
+while($param1>=$param2){
+$c = $a+$b;
+}
+;
+return ;
+}
 
 }
